74227c4e18e01f1283ee25391326f3f1df6d1e96
harryawk <harry.kambey@gmail.com>
2016-04-07 16:35:22 +0700

    TuitTuit Tweet Analyzer Project

---
+++ TuitTuit/TuitTuit/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace TuitTuit.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ TuitTuit/TuitTuit/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TuitTuit.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}

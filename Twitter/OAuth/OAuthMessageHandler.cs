1b51a00e535e4f22770e956a4b5dcd3d30d6c18a
monstergelo <bimawansatrianto@gmail.com>
2016-04-13 09:42:35 +0700

    project awal

---
+++ Twitter/OAuth/OAuthMessageHandler.cs
@@ -0,0 +1,51 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TwitterSample.OAuth
+{
+    /// <summary>
+    /// Basic DelegatingHandler that creates an OAuth authorization header based on the OAuthBase
+    /// class downloaded from http://oauth.net
+    /// </summary>
+    public class OAuthMessageHandler : DelegatingHandler
+    {
+        // Obtain these values by creating a Twitter app at http://dev.twitter.com/
+		private static string _consumerKey = "zS2WQkbuwOT07wxDPTontsBpv";
+		private static string _consumerSecret = "yocEMSKsubvLbyWAxgDKym9tNROyRnMfrc4Hk5EZZ7bpcq9LQU";
+		private static string _token = "279767182-jVrl4dR4PhwPUzNE3FwPmLjhpoX0HZEd4O59aBSe";
+		private static string _tokenSecret = "QXAlOyYsAZTVXnERIXiRnxslnkbUnQZoSOqzfuiRS8Pmj";
+
+        private OAuthBase _oAuthBase = new OAuthBase();
+
+        public OAuthMessageHandler(HttpMessageHandler innerHandler)
+            : base(innerHandler)
+        {
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // Compute OAuth header 
+            string normalizedUri;
+            string normalizedParameters;
+            string authHeader;
+
+            string signature = _oAuthBase.GenerateSignature(
+                request.RequestUri,
+                _consumerKey,
+                _consumerSecret,
+                _token,
+                _tokenSecret,
+                request.Method.Method,
+                _oAuthBase.GenerateTimeStamp(),
+                _oAuthBase.GenerateNonce(),
+                out normalizedUri,
+                out normalizedParameters,
+                out authHeader);
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("OAuth", authHeader);
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
---
+++ Twitter/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using TwitterSample.OAuth;
+using System.Threading.Tasks;
+
+namespace TwitterSample
+{
+    /// <summary>
+    /// Sample illustrating how to write a simple twitter client using HttpClient. The sample uses a 
+    /// HttpMessageHandler to insert the appropriate OAuth authentication information into the outgoing
+    /// HttpRequestMessage. The result from twitter is read as a JToken.
+    /// </summary>
+    /// <remarks>
+    /// Before you can run this sample you must obtain an application key from twitter, and 
+    /// fill in the information in the OAuthMessageHandler class, see 
+    /// http://dev.twitter.com/ for details.
+    /// </remarks>
+    class Program
+    {
+		private JToken statuses = null;
+        private static string _address = "https://api.twitter.com/1.1/statuses/user_timeline.json?include_rts=true&screen_name=scottgu&count=20";
+
+		private static async Task<JToken> RunClient()
+        {
+            // Create client and insert an OAuth message handler in the message path that 
+            // inserts an OAuth authentication header in the request
+            HttpClient client = new HttpClient(new OAuthMessageHandler(new HttpClientHandler()));
+
+            // Send asynchronous request to twitter and read the response as JToken
+            HttpResponseMessage response = await client.GetAsync(_address);
+			JToken statuses = null;
+           // if (response.IsSuccessStatusCode)
+           // {
+                statuses = await response.Content.ReadAsAsync<JToken>();
+               	/*
+				Console.WriteLine("Most recent statuses from ScottGu's twitter account:");
+                Console.WriteLine();
+                foreach (var status in statuses)
+                {
+                    Console.WriteLine("   {0}", status["text"]);
+                    Console.WriteLine();
+                }
+                */
+           // }
+			return statuses;
+        }
+
+        static void Main(string[] args)
+        {
+			JToken feed = null;
+			feed = RunClient().Result;
+
+
+			Console.WriteLine("Most recent statuses from ScottGu's twitter account:");
+			Console.WriteLine();
+			foreach (var status in feed)
+			{
+				Console.WriteLine("   {0}", status["text"]);
+				Console.WriteLine();
+			}
+
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+        }
+    }
+}

0ee299b86f9c238fd786e8e1ee456c8cfb5e81ad
kharisisriyant <kharisisriyant@gmail.com>
2016-04-23 15:09:24 +0700

    Perbaikan index sama result

---
+++ TwitWeb/try2/Controllers/ResultController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace try2.Controllers
+{
+    public class ResultController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
